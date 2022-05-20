50f798aaa97605b734ce8ff4f7998260bc3b3c6f
Ernest Gupik <ernestgupik@wp.pl>
2022-05-20 20:47:14 +0200

    Fix typo - Dependencyless -> Dependency-less

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # randlib
 //!
-//! Dependencyless random value generator using pointer addresses and time
+//! Dependency-less random value generator using pointer addresses and time
 //!
 //! The generator is using the LCG algorithm seed by a pointer address multiplied by
 //! UNIX_EPOCH.
